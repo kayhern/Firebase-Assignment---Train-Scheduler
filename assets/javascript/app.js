1c21353df5eaa4ada8dc7447ea43eb8d68a40df4
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-02 17:53:18 -0400

    changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 //Overview of homework: In this assignment, you'll create a train schedule application that incorporates Firebase to host arrival and departure data. Your app will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
 
 //my tutor advised to add this constant for moment.js to function
-const moment = require("moment");
+// const moment = require("moment");
+
+//borrowing functions form this class activity: https://harvard.bootcampcontent.com/Harvard-Coding-Boot-Camp/hu-cam-fsf-pt-09-2019-u-c/blob/master/Week_7/01-Activities/17-TimeSheet/Solved/timesheetLogic.js
 
 $(document).ready(function () {
     //Firebase configuration
@@ -44,7 +46,7 @@ $(document).ready(function () {
 
         //validating the train input fields are working
         console.log(newTrain.name);
-        console.log(newTrain.destination);
+        console.log(newTrain.place);
         console.log(newTrain.time);
         console.log(newTrain.frequency);
 
@@ -54,27 +56,40 @@ $(document).ready(function () {
         $("#firstTrainTime").val("");
         $("#Frequency").val("");
 
-
-
     });
 
-    //populating the html with train input from above and adding into firebase
+    //*populating the html with train input from above and adding into firebase
+
+    database.ref().on("child_added", function (childSnapshot) {
+        console.log(childSnapshot.val());
+
+        //variable time again
+        var name = childSnapshot.val().name;
+        var destination = childSnapshot.val().place;
+        var firstTrainTime = childSnapshot.val().time;
+        var frequency = childSnapshot.val().frequency;
+
+        //moment.js to calculate the minutes until arrival based upon the frequency and first start time
 
 
+        //new row on table
+        var newRow = $("<tr>").append(
+            $("<td>").text(name),
+            $("<td>").text(destination),
+            $("<td>").text(firstTrainTime),
+            $("<td>").text(frequency),
+            //this is the moment.js calculation
+            //  $("<td>").text(minutesAway),
+        );
 
+        //new role into the top table
+        $("#table-top > tbody").append(newRow);
 
-
-
-
-
-
+    });
 
 });
 
 
-
-
-//1. SM: incorporate moment.js
 //4. M: ensure website updates with data
 //5. L: ensure time data is altered properly (arrival time is relative to current time AND in military format)
 //https://momentjs.com/docs/#/displaying/difference/
